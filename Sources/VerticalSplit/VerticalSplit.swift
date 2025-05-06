7e49a2a277fa5a42959599cc96048522da3a57dc
Joseph Mbai <31988614+m0ahs@users.noreply.github.com>
2025-05-06 17:54:41 +0200

    Update VerticalSplit.swift

--- Sources/VerticalSplit/VerticalSplit.swift
+++ Sources/VerticalSplit/VerticalSplit.swift
@@ -179,7 +179,7 @@ public struct VerticalSplit<
                     newSplit = .fraction(0)
                 } else if newPartition > cardHeight - lil2 {
                     newPartition = cardHeight - lil
-                    newSplit = .bottomMini
+                    newSplit = .fraction(0.2)
                 } else if newPartition > cardHeight - lil3 {
                     newPartition = cardHeight - lil3
                     newSplit = .fraction(1)
